fca327e5212c606cce457607adb5418e1199f98b
nghugo <nghugoryan@gmail.com>
2026-07-28 00:33:15 +0100

    add iter

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 pub struct List<T> {
     head: Option<Box<Node<T>>>,
 }
@@ -20,7 +22,7 @@ impl<T> MyAsRef<T> for Option<T> {
             // 1. Rust sees self is `&Option<T>`
             // 2. Matching `Some(val)` auto-binds `val` as `&T` (borrowing)
             // 3. RHS `Some(val)` creates a BRAND NEW Option holding `&T`
-            Some(val) => Some(val)
+            Some(val) => Some(val),
         }
     }
 }
@@ -64,6 +66,43 @@ impl<T> Drop for List<T> {
     }
 }
 
+struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter { next: self.head.as_ref().map(|node| node.deref()) }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|next_node| next_node.deref());
+            &node.val
+        })
+    }
+}
+
+
 #[cfg(test)]
 mod test {
     use super::List;
@@ -91,6 +130,33 @@ mod test {
         assert_eq!(list.pop(), Some(2));
         assert_eq!(list.pop(), Some(9));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 }
 
